d50cc674919c6cd90b9f9b2c039abba9f6211adf
Quang Minh Nguyen <quangng711@outlook.com.au>
2026-07-04 14:05:42 +1000

    doing things w/ database table

--- BPapp/MainViewController.m
+++ BPapp/MainViewController.m
@@ -13,13 +13,33 @@
 @end
 
 @implementation MainViewController
+@synthesize systolicText, diatolicText, commentsText; // generate "setters" & "getters" for property
 
+- (void) createTable: (NSString *) tableName
+          withField1: (NSString *) field1
+          withField2: (NSString *) field2
+          withField3: (NSString *) field3
+          withField4: (NSString *) field4 {
+    // declaring table
+    char *err;
+    NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXISTS '%@' ('%@' "
+                     "TEXT PRIMARY KEY, '%@' INTEGER, '%@' INTEGER, '%@' TEXT);", tableName, field1, field2, field3, field4];
+    // create table
+    if (sqlite3_exec(db, [sql UTF8String] , NULL, NULL, &err) != SQLITE_OK) {
+        sqlite3_close(db);
+        NSAssert(0, @"Could not create table.");
+    } else {
+        NSLog(@"Table created");
+    }
+}
+// filepath to bp's db
 - (NSString *) filePath {
     NSArray *paths = NSSearchPathForDirectoriesInDomains //find where file is
     (NSDocumentDirectory, NSUserDomainMask, YES);
     return [[paths objectAtIndex:0] stringByAppendingPathComponent:@"bp.sql"]; //find the file, create if doesn't exist
 }
 
+// open db
 - (void)openDB {
     if (sqlite3_open([[self filePath] UTF8String], &db) != SQLITE_OK) {
         sqlite3_close(db);
@@ -32,6 +52,7 @@
 - (void)viewDidLoad
 {
     [self openDB];
+    [self createTable:@"summary" withField1:@"theDate" withField2:@"systolic" withField3:@"diastolic" withField4:@"comments"];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
